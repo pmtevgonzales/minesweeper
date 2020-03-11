6ce72f02eddbd69689d939ea63646575cb1408be
pmtevgonzales <patvalentinegonzales@gmail.com>
2020-03-11 22:42:41 +1300

    finished challenge 1

--- minesweeper.js
+++ minesweeper.js
@@ -6,7 +6,10 @@ function play(audioId) {
   audioFile.play();
 }
 
-var board = {}
+var board = {};
+
+var gameStarted = false;
+
 
 function startBoard (num) {
   board = {}
@@ -23,15 +26,29 @@ function startBoard (num) {
     }
   }
 
-  for (var i = 0; i < board.cells; i++) {
+  for (var i = 0; i < board.cells.length; i++) {
     var cell= board.cells[i];
     cell.surroundingMines = countSurroundingMines (cell)
   }
 
   startGame();
+  updateNotes();
   document.addEventListener("click", checkForWin);
   document.addEventListener("contextmenu", checkForWin);
+}
 
+function showResetGameBtn() {
+  var resetButton = document.getElementById("resetGame");
+  resetButton.classList.remove("hide");
+}
+
+function playCheerSound(event) {
+  //console.log(event.target.id);
+  play("cheerSound");
+}
+
+function playMarkedSound() {
+  play("markingBomb");
 }
 
 function hideIntro() {
@@ -41,15 +58,22 @@ function hideIntro() {
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
+  play("startGame");
   hideIntro();
-  lib.initBoard()
+  lib.initBoard();
+
+}
+
+// choose board to play again
+function resetBoard(){
+  location.reload();
 }
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin () {
+function checkForWin (event) {
   var mineCount = 0;
   var markedCount = 0;
   var hiddenCount = 0;
@@ -64,20 +88,44 @@ function checkForWin () {
     if (cell.hidden && !cell.isMarked) {
       hiddenCount++;
     }
+    if (!cell.hidden && cell.isMine) {
+      if (gameStarted) {
+        play("loseGame");
+      }
+      lib.displayMessage('You lose!');
+      showResetGameBtn();
+      gameStarted = false;
+      return;
+    }
+
+    if (!cell.hidden||cell.isMarked && !gameStarted) {
+      gameStarted = true;
+    }
+
   }
 
   if (mineCount == markedCount && hiddenCount == 0) {
-    play("winGame");
-    lib.displayMessage('You win!')
-    
+    if (gameStarted) {
+      play("winGame");
+    }
+    lib.displayMessage('You win!');
+    showResetGameBtn();
+    gameStarted = false;
+    return;
   }
+  if (event.type === "click" && gameStarted ) {
+    playCheerSound();
+  }
+  else if (event.type === "contextmenu" && gameStarted) {
+    playMarkedSound();
+  }
+
+}
+
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
-
-
-}
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
@@ -96,5 +144,22 @@ function countSurroundingMines (cell) {
     }
   }
   return mineCount;
+}
+
+function updateNotes() {
+  var notes = document.getElementById("notes");
+  var mineCount = 0;
+  for (var i = 0; i < board.cells.length; i++) {
+    var cell= board.cells[i];
+    if (cell.isMine) {
+      mineCount++;
+    }
+  }
+  notes.innerHTML = "There are "+mineCount+" bombs in the game!";
+}
+
+function clearNotes() {
+  var notes = document.getElementById("notes");
+  notes.innerHTML = "";
 }
 
